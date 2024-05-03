f8a4b5ace4d088bdb9e5022b275300414483be9c
emmanuelist <emmanuel.paul75@yahoo.com>
2024-05-03 12:33:56 +0100

    Implemented getLikedSongs function to fetch liked songs from Supabase using server-side components and Next.js headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+	images: {
+		//domains: ["ypoppodeangspytycuhz.supabase.co"]
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'ypoppodeangspytycuhz.supabase.co',
+				pathname: '/**',
+			},
+		],
+	}
+};
 
 export default nextConfig;
