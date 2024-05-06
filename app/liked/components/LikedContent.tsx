fccd3d4958870f416d59ac1d1bbf7dfbb6e3cfe4
emmanuelist <emmanuel.paul75@yahoo.com>
2024-05-06 14:24:59 +0100

    feat: add LikedContent component with user authentication and liked song display

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -6,6 +6,8 @@ import { useEffect } from "react";
 import { Song } from "@/types";
 import { useUser } from "@/hooks/useUser";
 import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LikedContentProps {
   songs: Song[];
@@ -15,6 +17,8 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
   const router = useRouter();
   const { isLoading, user } = useUser();
 
+  const onPlay = useOnPlay(songs);
+
   useEffect(() => {
     if (!isLoading && !user) {
       router.replace("/");
@@ -23,8 +27,17 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
 
   if (songs.length === 0) {
     return (
-      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-        No liked songs.
+      <div
+        className="
+            flex
+            flex-col
+            gap-y-2
+            w-full
+            px-6
+            text-neutral-400
+        "
+      >
+        No Liked songs!
       </div>
     );
   }
@@ -32,10 +45,11 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
   return (
     <div className="flex flex-col gap-y-2 w-full p-6">
       {songs.map((song) => (
-        <div key={song.id} className="flex items-center gp-x-4 w-full">
+        <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
