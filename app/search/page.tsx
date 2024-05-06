bd691950cd7f78326b71c20b5e583a3f96b99f51
emmanuelist <emmanuel.paul75@yahoo.com>
2024-05-06 14:41:00 +0100

    feat: add Library component with authentication, upload modal, and media item display

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -12,11 +12,10 @@ interface SearchProps {
 export const revalidate = 0;
 
 const Search = async ({ searchParams }: SearchProps) => {
-  //console.log(getSongsByTitle);
   const songs = await getSongsByTitle(searchParams.title);
 
   return (
-    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+    <div className="bg-slate-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header className="from-bg-neutral-900">
         <div className="mb-2 flex flex-col gap-y-6">
           <h1 className="text-white text-3xl font-semibold">Search</h1>
