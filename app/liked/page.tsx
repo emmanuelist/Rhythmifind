a91a4d84ef3c1fd5f9cdec424a43a5587968186f
emmanuelist <emmanuel.paul75@yahoo.com>
2024-05-06 14:27:14 +0100

    feat: add Liked page with Header and LikedContent components

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -9,7 +9,7 @@ export const revalidate = 0;
 const Liked = async () => {
   const songs = await getLikedSongs();
   return (
-    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+    <div className="bg-slate-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
         <div className="mt-20">
           <div className="flex flex-col md:flex-row items-center gap-x-5">
@@ -23,14 +23,14 @@ const Liked = async () => {
             </div>
             <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
               <p className="hidden md:block font-semibold text-sm">Playlist</p>
-              <h1 className="text-white text-4xl sm:text-5xl lg:ext-7xl font-bold">
+              <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold">
                 Liked Songs
               </h1>
             </div>
           </div>
         </div>
       </Header>
-	  <LikedContent songs={songs} />
+      <LikedContent songs={songs} />
     </div>
   );
 };
