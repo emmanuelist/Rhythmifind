f229c7be0822f326172fb001197c017c85314762
Godwin Chukwudi Chukwuma <106426176+GodwinCyber@users.noreply.github.com>
2024-05-16 10:52:23 +0100

    Provider component for the entire design, it include:
    - ModalProvider: Render MyUserContextProvider with children
    - SupabaseProvider: Props for SupabaseProvider component
    - ToasterProvider: ToasterProvider component provides custom styling
    - UserProvider: UserProvider component wraps the application with the user context provider

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,26 +1,34 @@
 "use client";
 
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"; // Import function to create Supabase client
+import { SessionContextProvider } from "@supabase/auth-helpers-react"; // Import SessionContextProvider
+import { useState } from "react"; // Import useState hook
 
-import { Database } from "@/types_db";
+import { Database } from "@/types_db"; // Import Database type
 
+/** 
+ * Props for SupabaseProvider component
+ * SupabaseProvider component wraps children components with Supabase session context
+ * Provide Supabase client to SessionContextProvider
+ * Provide Supabase client to SessionContextProvider
+ * Render children component
+*/
 interface SupabaseProviderProps {
-  children: React.ReactNode;
+  children: React.ReactNode; // Children components
 }
 
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
   children
 }) => {
   const [supabaseClient] = useState(() =>
-    createClientComponentClient<Database>()
+    createClientComponentClient<Database>() // Create Supabase client
   );
+  
   return (
-    <SessionContextProvider supabaseClient={supabaseClient}>
-      {children}
+    <SessionContextProvider supabaseClient={supabaseClient}> 
+      {children} 
     </SessionContextProvider>
   );
 };
 
-export default SupabaseProvider;
+export default SupabaseProvider; // Export SupabaseProvider component
